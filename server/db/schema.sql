31e48fef280806b8a691b600032560b09eb58a98
Brianna Kincart <briannakincart@gmail.com>
2021-04-13 18:46:20 -0400

    apprenti 5 day one final code

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,1 +1,9 @@
 -- Create your schema here
+DROP TABLE IF EXISTS songs;
+
+CREATE TABLE songs (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  movie VARCHAR(255) NOT NULL,
+  length FLOAT NOT NULL
+);
--- server/routes/api/v1/songsRouter.js
+++ server/routes/api/v1/songsRouter.js
@@ -4,6 +4,39 @@ import Song from "../../../models/Song.js"
 
 const songsRouter = express.Router()
 
+songsRouter.get("/", async (req, res) => {
+  try {
+    // get all of the songs from the database
+    // (via the model!)
+    // we need to await this so that `findAll` has time to query the database
+    const songs = await Song.findAll()
+    // console.log(songs)
+  
+    // send that data as a JSON response
+    res.json({ songs: songs })
+    
+  } catch(err) {
+    // if anything goes wrong,
+    // catch it and console.log(errors), and respond with the error
+    console.log(err)
+    res.status(500).json({ error: err })
+  }
+})
 
+songsRouter.get("/:id", async (req,res) => {
+  try {
+    const id = req.params.id
+    // I want to take the id from my URL
+    // and get the record in the database with the matching id
+    // (I'm going to do this using my model)
+    const singleSong = await Song.findById(id)
+
+    // send that data for the single song as a JSON response
+    res.json({ song: singleSong })
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ error: err })
+  }
+})
 
 export default songsRouter
